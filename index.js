a3037d46c4cbe3311f3ce41ad7394271e3bc1ac8
Lucy Bellott <lbellott@tripadvisor.com>
2024-04-05 12:20:53 +0200

    finished

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 //code
 
-console.log("hello world")
+function randomChoice() {
+    
+    let choice = Math.floor(Math.random() * 3)
+    // console.log(choice)
+    
+    if(choice === 0){
+        return "rock"
+    }
+    else if(choice === 1){
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+function winner (player1, player2) {
+
+    if(player1 === "rock" && player2 === "scissors" || player1 === "paper" && player2 === "rock"  || player1 === "scissors" && player2 === "paper"){
+            return "Player 1 wins!"
+        }
+        else if (player1 === player2){
+            return "it's a tie :("
+        }
+        else {
+            return "Player 2 wins!"
+        }
+
+}
+
+let player1 = randomChoice()
+let player2 = randomChoice()
+let result = winner(player1, player2)
+
+
+//console.log(randomChoice())
+console.log("player 1: ", player1)
+console.log("player 2: ", player2)
+console.log(result)
+
